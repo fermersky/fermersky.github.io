1f9f58ae065ba686c24a80ea11ed48c13b0b9186
fermersky <forte201253@gmail.com>
2020-07-22 15:04:22 +0300

    react client

---
+++ precache-manifest.921807cc9978532f4d8901854f2f755d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2127e5be67c97a99c5ff4ef843005a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "893535ebfa4f2606bee4",
+    "url": "/static/css/main.ee58a8ae.chunk.css"
+  },
+  {
+    "revision": "ee0a71d801fe4b83c2ed",
+    "url": "/static/js/2.6edd6ca1.chunk.js"
+  },
+  {
+    "revision": "78b287351bb08f453fa0918ea10ac39c",
+    "url": "/static/js/2.6edd6ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "893535ebfa4f2606bee4",
+    "url": "/static/js/main.0a9466fb.chunk.js"
+  },
+  {
+    "revision": "53618f2d601525758475",
+    "url": "/static/js/runtime-main.046ff16f.js"
+  }
+]);
