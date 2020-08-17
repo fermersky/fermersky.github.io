455ebac5d48435da85e15afbb9ffb6fcf72cd34e
fermersky <forte201253@gmail.com>
2020-08-17 14:25:39 -0600

    fixes

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c639c59abfb3d69f0c9057f0d06b29b8.js"
+  "/precache-manifest.25e159e651496507f53a78b327e47287.js"
 );
 
 self.addEventListener('message', (event) => {
